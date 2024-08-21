e4943bd8ff7da04c3bde9282a49f3fa35027cd75
menezesmar <mmgestudos@gmail.com>
2024-08-21 09:21:29 +0100

    Initial commit

---
+++ src/main/java/br/com/iterasys/Medidas.java
@@ -0,0 +1,30 @@
+// 1 - Pacote
+package br.com.iterasys;
+
+// 2 - Referência as bibliotecas
+
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Caracteristicas
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args) {
+        System.out.println("Cálculo de Áreas");
+
+// Cálculo de Áreas - Reduzido
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento +
+                "m a área é de " + largura * comprimento + "m²");
+    }
+
+    public void calcularAreaModoExtenso(){
+        // Cálcudo de Área - Exemplo: o tamanho do tapete ou do piso
+        
+    }
+
+
+
+    }
